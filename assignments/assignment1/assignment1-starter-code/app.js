88a943b9338f09deca1d659618a2156f058e805f
shashi kumar <shashikumar30030@gmail.com>
2018-08-18 14:06:06 +0530

    clean code

--- assignments/assignment1/assignment1-starter-code/app.js
+++ assignments/assignment1/assignment1-starter-code/app.js
@@ -5,14 +5,15 @@
   angular.module('LunchCheck',[])
   .controller("LunchCheckController",function($scope){
     $scope.errorMessage = "";
+    $scope.items = "";
     //function to perform action on button click
     $scope.checkIfTooMuch = function(){
-      if($scope.items === undefined||$scope.items ===""){
+      if($scope.items ===""){
         $scope.message = null;
         $scope.errorMessage = "enter food list first";
         return;
       }
-      $scope.errorMessage = "";
+      $scope.errorMessage = null;
       var spiltRegExp = /\s*,[\,,\s ]*\s*/;
       var itemsString = $scope.items.replace(/^,*|,*$/g,'');
       var itemArr = itemsString.split(spiltRegExp);
