c82c81990244a4a596efb3d286e8b4db67c52988
shashi kumar <shashikumar30030@gmail.com>
2018-09-10 00:04:57 +0530

    removing of log statement

--- assignments/assignment 4/menu/items/items.controller.js
+++ assignments/assignment 4/menu/items/items.controller.js
@@ -6,16 +6,9 @@ angular.module('MenuApp')
 
 itemsController.$inject = ['items'];
 
-function itemsController(items) {
-  var $ctrl = this;
-  console.log("sararara");
-  console.log("lenght ="+items.menu_items.length);
-  $ctrl.itemslist = items;
-    console.log("sararara1"+$ctrl.itemslist.menu_items.length);
-  for(var i=0;i<$ctrl.itemslist.menu_items.length;i++){
-    console.log("itemsController ="+$ctrl.itemslist.menu_items[i].name);
+  function itemsController(items) {
+    var $ctrl = this;
+    $ctrl.itemslist = items;
   }
-    console.log("sararara2");
-}
 
 })();
--- assignments/assignment 4/menu/service/menudata.service.js
+++ assignments/assignment 4/menu/service/menudata.service.js
@@ -13,7 +13,6 @@ function MenuDataService($http){
       methord : "GET",
       url : "https://davids-restaurant.herokuapp.com/categories.json"
     }).then(function(categoriesList){
-       console.log(categoriesList.data);
        return categoriesList.data;
     },function(error){
       console.log("error occure while featching categoriesList");
@@ -21,14 +20,11 @@ function MenuDataService($http){
   };
 
   thisService.getItemsForCategory = function(categoryShortName){
-    console.log("getItemsForCategory =" +categoryShortName);
     return $http({
       methord : "GET",
       url : " https://davids-restaurant.herokuapp.com/menu_items.json",
       params : {'category':categoryShortName}
     }).then(function(menuItems){
-      console.log("data");
-       console.log(menuItems.data);
        return menuItems.data;
     },function(error){
       console.log("error occure while featching menuItems");
